69f88813adb74244a0cebb4ddd2611054e62e23f
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-03-18 21:01:38 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,8 @@
 def alphabetize(arr)
-  esp_alph = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
-  arr.sort_by do |string|
-    string.tr(esp_alph, ascii)
-end
+  esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  arr.sort_by do |word|
+    word.split('').collect do |letter|
+      esperanto_alphabet.index(letter)
+    end
+  end
 end
